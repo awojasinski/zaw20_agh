f7f3250ec91e62351e652e6d7b887650656b1599
awojasinski <a.wojasinski@gmail.com>
2020-03-23 08:40:58 +0100

    additional exercise - background model initilalization

---
+++ lab3/lab3_additional_1.py
@@ -0,0 +1,132 @@
+import cv2 as cv
+import numpy as np
+
+# iteracja po każdym zbiorze obrazów
+for img_source in ['highway', 'office', 'pedestrants']:
+    kernel = np.ones((2, 2), np.int)
+    threshold_value = 50
+    buffer_size = 60
+
+    f = open(img_source + '/temporalROI.txt', 'r')   # otwarcie pliku
+    line = f.readline()     # odczyt lini
+    roi_start, roi_end = line.split()   # rozbicie lini na poszczegolne
+
+    roi_start = 1000    #int(roi_start)
+    roi_end = int(roi_end)
+    step_frame = 1
+    alpha = 0.01  # waga nowej klatki w obliczniu modelu
+
+    # zmienne do pomiaru dokładności algorytmu
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    # sprawdzenie rozdzielczości obrazu
+    I = cv.imread(img_source + '/input/in%06d.jpg' % roi_start)
+    YY = I.shape[0]  # wysokość
+    XX = I.shape[1]  # szerokość
+    Buffer = np.zeros(shape=(YY, XX, buffer_size), dtype=np.uint8)
+    I_model = np.zeros(shape=(YY, XX), dtype=np.uint8)
+
+    for n, img in enumerate(range(roi_start-buffer_size, roi_start, step_frame)):
+        I = cv.imread(img_source + '/input/in%06d.jpg' % img)
+        I_G = cv.cvtColor(I, cv.COLOR_BGR2GRAY)
+        Buffer[:, :, n] = I_G
+
+    # inicjalizacja modelu tła
+    I_model = np.zeros((YY, XX), np.uint8)
+    for i in range(YY):
+        for j in range(XX):
+            counts = np.bincount(Buffer[i, j, :])
+            I_model[i, j] = np.argmax(counts)
+
+    I_model_prev = I_model
+    buff = 0
+    for i in range(roi_start, roi_end, step_frame):
+
+        I = cv.imread(img_source + '/input/in%06d.jpg' % i)
+        I_GT = cv.imread(img_source + '/groundtruth/gt%06d.png' % i)
+
+        I_G = cv.cvtColor(I, cv.COLOR_BGR2GRAY)
+        I_GT = cv.cvtColor(I_GT, cv.COLOR_BGR2GRAY)
+
+        # Aproksymacja medianowa
+        if I_model.any() < I_G.any():
+            I_model = I_model + 1
+        elif I_model.any() > I_G.any():
+            I_model = I_model - 1
+        else:
+            I_model = I_model
+
+        # konserwatywne podejście do aktualizacji modelu tła
+        if 'I_B' in globals():  # sprawdzenie czy istnieje taka zmienna
+            I_model = np.where(I_B == 0, I_model, I_model_prev)  # aktualizacja tylko w miejscach gdzie było tło
+        I_mov = cv.absdiff(I_G, I_model)
+
+        I_model_prev = I_model
+
+        I_B = 1 * (I_mov > threshold_value)  # konwersja typu logicznego na liczbowy
+        I_B = I_B * 255  # zamiana zakresu z {0, 1} na {0, 255} (poprawne wyświetlanie)
+        I_B = I_B.astype(np.uint8)  # rzutowanie zmiennych na typ całkowity uint8
+
+        I_GT = 1 * (I_GT == 255)  # konwersja typu logicznego na liczbowy
+        I_GT = I_GT * 255  # zamiana zakresu z {0, 1} na {0, 255} (poprawne wyświetlanie)
+        I_GT = I_GT.astype(np.uint8)  # rzutowanie zmiennych na typ całkowity uint8
+
+        I_B = cv.medianBlur(I_B, 3)
+        # operacje morfologiczne na analizowanym obrazie
+        I_B = cv.erode(I_B, kernel, iterations=1)
+        I_B = cv.dilate(I_B, kernel, iterations=1)
+
+        retval, labels, stats, centroids = cv.connectedComponentsWithStats(I_B)
+
+        # rysowanie prostokąta wokół największego ruchomego obiektu
+        I_VIS = I.copy()
+        if stats.shape[0] > 1:
+            tab = stats[1:, 4]
+            pi = np.argmax(tab)
+            pi = pi + 1
+            cv.rectangle(I_VIS, (stats[pi, 0], stats[pi, 1]),
+                         (stats[pi, 0] + stats[pi, 2], stats[pi, 1] + stats[pi, 3]), (255, 0, 0), 2)
+            cv.putText(I_VIS, "%f" % stats[pi, 4], (stats[pi, 0], stats[pi, 1]), cv.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (255, 0, 0))
+            cv.putText(I_VIS, "%d" % pi, (np.int(centroids[pi, 0]), np.int(centroids[pi, 1])), cv.FONT_HERSHEY_SIMPLEX,
+                           1, (255, 0, 0))
+
+        # połączenie trzech obrazów w jeden (łatwiejsza analiza przez użytkownika, mniejszy bałagan)
+        labels_img = cv.cvtColor(np.uint8(labels / retval * 255), cv.COLOR_GRAY2BGR)
+        I_GT_bgr = cv.cvtColor(I_GT, cv.COLOR_GRAY2BGR)
+        video = cv.hconcat([labels_img, I_VIS, I_GT_bgr])
+        cv.imshow('Video: median method', video)
+
+        TP_M = np.logical_and((I_B == 255), (I_GT == 255))
+        TP_S = np.sum(TP_M)
+        TP = TP + TP_S
+
+        TN_M = np.logical_and((I_B == 0), (I_GT == 0))
+        TN_S = np.sum(TN_M)
+        TN = TN + TN_S
+
+        FP_M = np.logical_and((I_B == 255), (I_GT == 0))
+        FP_S = np.sum(FP_M)
+        FP = FP + FP_S
+
+        FN_M = np.logical_and((I_B == 0), (I_GT == 255))
+        FN_S = np.sum(FN_M)
+        FN = FN + FN_S
+
+        cv.waitKey(10)
+
+    del globals()['I_B']    # usunięcie zmiennej będącej obrazem binarnym z wykrytymi obiektami pierwszoplanowymi
+    cv.destroyAllWindows()
+
+    P = TP / (TP + FP)
+    R = TP / (TP + FN)
+    F1 = (2 * P * R) / (P + R)
+
+    print(img_source)
+    print('Precyzja: ', P)
+    print('Czułość: ', R)
+    print('F1: ', F1)
+    print()
