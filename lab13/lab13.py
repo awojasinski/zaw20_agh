db155628a2e4afaacbfb5069dcf7815fbab4bafd
Adam Wojasinski <adam.wojasinski@aptiv.com>
2020-06-15 11:30:25 +0200

    refactoring code - now algorithm works as should

--- lab13/lab13.py
+++ lab13/lab13.py
@@ -1,84 +1,97 @@
 import cv2 as cv
-import numpy as np
 import math
-import matplotlib.pyplot as plt
-from scipy.ndimage.measurements import center_of_mass as center_of_mass
+import numpy as np
+from scipy.spatial import distance
 
-kernel_size = 45
+kernel_size1 = 50 # rozmiar rozkladu
+kernel_size2 = 80 # rozmiar rozkladu
 mouseX, mouseY = (830, 430)
 
+sigma = kernel_size1 / 6  # odchylenie std
+x = np.arange(0, kernel_size1, 1, float)  # wektor poziomy
+y = x[:, np.newaxis]  # wektor pionowy
+x0 = y0 = kernel_size1 // 2  # wsp. srodka
+G1 = 1 / (2 * math.pi * sigma ** 2) * np.exp(-0.5 * ((x - x0) ** 2 + (y - y0) ** 2) / sigma ** 2)
 
-def track_init(event, x, y, flag, param):
+sigma = kernel_size2 / 5  # odchylenie std
+x = np.arange(0, kernel_size2, 1, float)  # wektor poziomy
+y = x[:, np.newaxis]  # wektor pionowy
+x0 = y0 = kernel_size2 // 2  # wsp. srodka
+G2 = 1 / (2 * math.pi * sigma ** 2) * np.exp(-0.5 * ((x - x0) ** 2 + (y - y0) ** 2) / sigma ** 2)
+
+
+def track_init(event, x, y, flags, param):
     global mouseX, mouseY
     if event == cv.EVENT_LBUTTONDBLCLK:
-        cv.rectangle(param, (x-kernel_size//2, y-kernel_size//2), (x + kernel_size//2, y + kernel_size//2), (0, 255, 0), 2)
+        cv.rectangle(param, (x-kernel_size1//2, y-kernel_size1//2), (x + kernel_size1//2, y + kernel_size1//2), (0, 255, 0), 2)
         mouseX, mouseY = x, y
 
-def gen_gauss(kernel_size, mouseX, mouseY, I):
-    # Generowanie Gaussa
-    sigma = kernel_size / 6  # odchylenie std
-    x = np.arange(0, kernel_size, 1, float)  # wektor poziomy
-    y = x[:, np.newaxis]  # wektor pionowy
-    x0 = y0 = kernel_size // 2  # wsp. srodka
-    G = 1 / (2 * math.pi * sigma ** 2) * np.exp(-0.5 * ((x - x0) ** 2 + (y - y0) ** 2) / sigma ** 2)
 
-    xS = mouseX - kernel_size // 2
-    yS = mouseY - kernel_size // 2
+def gauss_hist(I_H_roi, G):
+    hist_q = np.zeros((256, 1), float)
+    for u in range(256):
+        mask = I_H_roi == u
+        hist_q[u] = np.sum(G[mask])
 
+    hist_q = hist_q / np.max(hist_q)
+    return hist_q
+
+
+def findPos(x, y, I_H, hist):
+    while True:
+        xS = x - kernel_size2 // 2
+        yS = y - kernel_size2 // 2
+
+        I_H_roi = I_H[yS:yS + kernel_size2, xS:xS + kernel_size2]
+
+        hist_new = gauss_hist(I_H_roi, G2)
+        hist_mul = np.sqrt(hist * hist_new)
+
+        mask = np.zeros((kernel_size2, kernel_size2), float)
+        for ii in range(0, kernel_size2):
+            for jj in range(0, kernel_size2):
+                mask[ii, jj] = hist_mul[I_H_roi[ii, jj]]
+        mask = mask*G2
+
+        M = cv.moments(mask)
+        imgX = int(M["m10"] / M["m00"])
+        imgY = int(M["m01"] / M["m00"])
+
+        newX = xS + imgX
+        newY = yS + imgY
+        dist = distance.euclidean((newX, newY), (x,y))
+        if dist < 2:
+            break
+        x = newX
+        y = newY
+
+    return newX, newY, hist_new
+
+
+for i in range(100, 200):
+    img_name = 'track_seq/track00' + str(i) + '.png'
+    I = cv.imread(img_name)
     I_HSV = cv.cvtColor(I, cv.COLOR_BGR2HSV)
     I_H = I_HSV[:, :, 0]
-    hist_q = np.zeros((256, 1), float)
-    for jj in range(0, kernel_size):
-        for ii in range(0, kernel_size):
-            pixel_H = I_H[yS + jj, xS + ii]
-            hist_q[pixel_H] += G[jj, ii]
+    if i == 100:
+        cv.namedWindow('Tracking')
+        cv.setMouseCallback('Tracking', track_init, param=I)
 
-    hist_q = hist_q / np.amax(hist_q)
-    I_H_part = I_H[yS : yS + kernel_size, xS : xS + kernel_size]
-    return hist_q, I_H_part
-
-
-# Wczytanie pierwszego obrazka
-I = cv.imread('track_seq/track00100.png')
-cv.namedWindow('Tracking')
-cv.setMouseCallback('Tracking', track_init, param=I)
-
-
-# Pobranie klawisza
-while(1):
-    cv.imshow('Tracking', I)
-    k = cv.waitKey(20) & 0xFF
-    if k == 27:
-        break
-
-hist_q_1, I_H_1 = gen_gauss(kernel_size=kernel_size, mouseX=mouseX, mouseY=mouseY, I=I)
-yS = mouseY
-xS = mouseX
-
-for i in range(101, 200):
-    img_name = 'track_seq/track00' + str(i) + '.png'
-    I_next = cv.imread(img_name)
-    cv.rectangle(I_next, (xS - kernel_size // 2, yS - kernel_size // 2), (xS + kernel_size // 2, yS + kernel_size // 2),
-                  (0, 255, 0), 2)
-
-    hist_q_2, I_H_2 = gen_gauss(kernel_size=kernel_size, mouseX=xS, mouseY=yS, I=I_next)
-
-    x_na_potem = xS - kernel_size // 2
-    y_na_potem = yS - kernel_size // 2
-
-    bhatta = np.sqrt(hist_q_2 * hist_q_1)
-    I = I_next
-
-    Wynik = np.zeros((kernel_size, kernel_size), dtype=float)
-    for jj in range(0, kernel_size):
-        for ii in range(0, kernel_size):
-            pixel_H = I_H_2[jj, ii]
-            Wynik[jj, ii] = bhatta[pixel_H] * hist_q_2[pixel_H]
-
-    xS, yS = center_of_mass(Wynik)
-    xS = int(xS + x_na_potem)
-    yS = int(yS + y_na_potem)
-
-    print(xS, yS)
-    cv.imshow('Tracking', I_next)
-    cv.waitKey(10)
+        # Pobranie klawisza
+        while (1):
+            cv.imshow('Tracking', I)
+            k = cv.waitKey(20) & 0xFF
+            if k == 27:
+                cv.destroyAllWindows()
+                break
+        x = mouseX
+        y = mouseY
+        xS = x - kernel_size1 // 2
+        yS = y - kernel_size1 // 2
+        I_H_roi = I_H[yS:yS+kernel_size1, xS:xS+kernel_size1]
+        hist = gauss_hist(I_H_roi, G1)
+    else:
+        x, y, hist = findPos(x, y, I_H, hist)
+    cv.rectangle(I, (x-kernel_size1//2, y-kernel_size1//2), (x+kernel_size1//2, y+kernel_size1//2), (0, 255, 0), 2)
+    cv.imshow('Ttacking', I)
+    cv.waitKey(50)
